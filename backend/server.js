9f8b6682395d6090b34ea8053a7d927ab68841e8
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2024-10-08 21:23:18 +0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,160 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+// const db = "mongodb://localhost:27017/test5";
+const db =
+  "mongodb+srv://alihassanhaedr:c4a@cluster0.ue5ezcc.mongodb.net/2024blog?retryWrites=true&w=majority";
+const port = process.env.PORT || 4000;
+const cookieParser = require("cookie-parser");
+const UserModel = require("./model/UserModel");
+const PostModel = require("./model/PostModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+const path = require("path");
+
+const secret = "lkfsjdlioglsfsnflkj";
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+// main app
+// auth
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userDoc = await UserModel.create({
+      username,
+      password: bcrypt.hashSync(password, 10),
+    });
+
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const userDoc = await UserModel.findOne({ username });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (passOk) {
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token, { sameSite: "none", secure: true }).json({
+          id: userDoc._id,
+          username,
+        });
+      });
+    } else {
+      return res.status(400).json("wrong credintioals");
+    }
+  } else {
+    return res.status(400).json("user not found");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+// posts
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await PostModel.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await PostModel.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await PostModel.find({})
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await PostModel.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+// end main app
+
+app.all("*", (req, res) => {
+  res.json({ message: "404 Not Found" });
+});
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
